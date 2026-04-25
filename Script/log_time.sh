554fa9d111b1f605579b3f901f37d1244bbaa60d
Nadila juniarti <250306028.mhs@uinmataram.ac.id>
2026-04-25 13:51:11 +0700

    Create log_time.sh

---
+++ Script/log_time.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+date >> log_time.txt
+
+#!/bin/bash → menentukan script dijalankan dengan Bash
+date → mengambil waktu saat ini
+>> log_time.txt → menyimpan hasil ke file (ditambah terus, tidak ditimpa)
